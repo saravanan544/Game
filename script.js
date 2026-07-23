030f64d092c49593ee2bf940ceaf124ab54c9698
Lucifer <saravanalingam544@gmail.com>
2026-07-23 16:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,5 @@
-// ======================
-// FLOOR 1 MONSTERS
-// ======================
+document.getElementById("enterBtn").onclick = function(){
 
-const floor1Monsters = [
+    location.href = "tower.html";
 
-{
-    name: "Wolf",
-    hp: 30,
-    maxHp: 30,
-    atk: 7,
-    def: 2,
-    image: "images/wolf.png"
-},
-
-{
-    name: "Goblin Scout",
-    hp: 35,
-    maxHp: 35,
-    atk: 6,
-    def: 4,
-    image: "images/goblin.png"
-},
-
-{
-    name: "Mushroom Slime",
-    hp: 45,
-    maxHp: 45,
-    atk: 4,
-    def: 6,
-    image: "images/slime.png"
-}
-
-];
+};
