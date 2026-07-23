5bee314a049e11f9cd535b77d8544ff946008d4b
Lucifer <saravanalingam544@gmail.com>
2026-07-23 17:13:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ document.getElementById("enterBtn").onclick = function(){
     location.href = "tower.html";
 
 };
+document.querySelector(".unlocked").onclick = function () {
+    location.href = "battle.html";
+};
